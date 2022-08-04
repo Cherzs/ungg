244dffb4032b2dfe600b044de93bc872159e5039
Ghaly <76243151+Cherzs@users.noreply.github.com>
2022-08-04 16:05:49 +0700

    Create sayang.cpp

---
+++ sayang.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+  
+  int a;
+  int b;
+  
+  cout << "Masukan nilai :";
+  cin >> a;
+  cout << "Nilai dari a adalah: \n";
+  
+  cin.get()
+  return 0;
+}
